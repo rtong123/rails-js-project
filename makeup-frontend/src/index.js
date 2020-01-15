85ac15548cbed952965e7afcde3e32bd1ef01c45
Rebecca Tong <rtong@buffalo.edu>
2020-01-15 17:30:25 -0500

    adding to onclick

--- makeup-frontend/src/index.js
+++ makeup-frontend/src/index.js
@@ -7,32 +7,35 @@ function submitProduct(){
     price: price,
     category: category
   }
+}
 //saving everything as an object.
   // next steps:
-  // implement backend code for CREATE products
-  // fetch('http://localhost:3000/products')
-  // .then(response => response.json())
-  // .then(myJson => myJson)
+  // implement backend code for CREATE productsi
+  function showProduct(){
+  fetch('http://localhost:3000/products')
+  .then(response => response.json())
+  .then(myJson => console.log(myJson[0]))
+}
 
-
-  fetch('http://localhost:3000/products', {
-    method: 'POST', // or 'PUT'
-    body: JSON.stringify(data),
-  })
-  .then((response) => response.json())
-  .then((data) => {
-    console.log('Success:', data);
-  })
-  .catch((error) => {
-    console.error('Error:', error);
-  });
+  // fetch('http://localhost:3000/products', {
+  //   method: 'POST', // or 'PUT'
+  //   body: JSON.stringify(data),
+  // })
+  // .then((response) => response.json())
+  // .then((data) => {
+  //   console.log('Success:', data);
+  //   //append to the dom?
+  //   console.log(@products)
+  // })
+  // .catch((error) => {
+  //   console.error('Error:', error);
+  // });
 
   //when they click submit, they can see all the products
-  let f = getElementById("form")
-  f.getElementById("button").addEventListener(function() {
-  console.log('hi')
-  });
-}
+
+
+
+
 
 
 
