d7eb344b7edc9171b4f4ded14bc6ac49e949c546
Rebecca Tong <rtong@buffalo.edu>
2020-01-15 14:51:52 -0500

    adding event handler for button

--- makeup-frontend/src/index.js
+++ makeup-frontend/src/index.js
@@ -10,10 +10,30 @@ function submitProduct(){
 //saving everything as an object.
   // next steps:
   // implement backend code for CREATE products
-  fetch('http://localhost:3000/products')
-  .then(response => response.json())
-  .then(myJson => myJson)
+  // fetch('http://localhost:3000/products')
+  // .then(response => response.json())
+  // .then(myJson => myJson)
+
+
+  fetch('http://localhost:3000/products', {
+    method: 'POST', // or 'PUT'
+    body: JSON.stringify(data),
+  })
+  .then((response) => response.json())
+  .then((data) => {
+    console.log('Success:', data);
+  })
+  .catch((error) => {
+    console.error('Error:', error);
+  });
+
+  //when they click submit, they can see all the products
+  document.getElementById("button").attachEvent(function() { /* do stuff here*/
+  console.log('hi')
+  });
 }
+
+
 
   // fetch('http://localhost:3000/products', {
   //   method: 'POST', // or 'PUT'
