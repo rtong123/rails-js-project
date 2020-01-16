444547037d59be70e457980a4d333dd53c72e21e
Rebecca Tong <rtong@buffalo.edu>
2020-01-15 23:28:13 -0500

    added to js file

--- makeup-frontend/src/index.js
+++ makeup-frontend/src/index.js
@@ -1,36 +1,39 @@
 function submitProduct(){
   const name = document.getElementById("name").value
   const price = document.getElementById("price").value
+  const brand = document.getElementById("brand").value
+  const category = document.getElementById("category").value
+  // saving everything as an object.
   const data = {
     name: name,
     brand: brand,
     price: price,
     category: category
   }
-}
-//saving everything as an object.
+  console.log(data)
   // next steps:
-  // implement backend code for CREATE productsi
-  function showProduct(){
-  fetch('http://localhost:3000/products')
-  .then(response => response.json())
-  .then(myJson => console.log(myJson[0]))
+  // implement backend code for CREATE products
+//   fetch('http://localhost:3000/products')
+//   .then(response => response.json())
+//   .then(myJson => console.log(myJson))
+// }
+
+  fetch('http://localhost:3000/products', {
+    method: 'POST',
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(data),
+  })
+  .then((response) => response.json())
+  .then((data) => {
+    console.log('Success:', data);
+    //append to the dom?
+  })
+  .catch((error) => {
+    console.error('Error:', error);
+  });
 }
-
-  // fetch('http://localhost:3000/products', {
-  //   method: 'POST', // or 'PUT'
-  //   body: JSON.stringify(data),
-  // })
-  // .then((response) => response.json())
-  // .then((data) => {
-  //   console.log('Success:', data);
-  //   //append to the dom?
-  //   console.log(@products)
-  // })
-  // .catch((error) => {
-  //   console.error('Error:', error);
-  // });
-
   //when they click submit, they can see all the products
 
 
