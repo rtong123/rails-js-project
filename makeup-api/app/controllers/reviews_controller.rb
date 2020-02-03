48c16907c644cb14c4545aebc1f25975c9c4fc4c
Rebecca Tong <rtong@buffalo.edu>
2020-02-03 16:25:08 -0500

    created migration;

--- makeup-api/app/controllers/reviews_controller.rb
+++ makeup-api/app/controllers/reviews_controller.rb
@@ -17,6 +17,6 @@ class ReviewsController < ApplicationController
 
   private
   def review_params
-    params.permit(:name,:brand,:price,:category)
+    params.permit(:title,:content,:category)
   end
 end
--- makeup-api/db/schema.rb
+++ makeup-api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_19_201334) do
+ActiveRecord::Schema.define(version: 2020_02_02_194648) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2020_01_19_201334) do
     t.string "category"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "product_id"
   end
 
 end
