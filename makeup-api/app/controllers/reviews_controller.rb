e4bf09d34228f2198e45b1063c9af5ed87255136
Rebecca Tong <rtong@buffalo.edu>
2020-02-21 18:28:06 -0500

    fix backend

--- makeup-api/app/controllers/reviews_controller.rb
+++ makeup-api/app/controllers/reviews_controller.rb
@@ -6,8 +6,11 @@ class ReviewsController < ApplicationController
 
   # implement code for create (POST) endpoint
   def create
+    @product = Product.find_by(id: params[:product_id])
+
     @review = Review.new(review_params)
     @review.save
+
     # why isnt the review being saved
     render json: @review
   end
--- makeup-api/app/models/review.rb
+++ makeup-api/app/models/review.rb
@@ -1,3 +1,4 @@
 class Review < ApplicationRecord
   # by taking out the assocations it saved
+
 end
